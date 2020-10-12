58879264a2aee477a91900fdaf9e2f96a3c6e47e
FantaCola49 <49249521+FantaCola49@users.noreply.github.com>
2020-10-12 19:07:24 +0300

    Добавьте файлы проекта.

---
+++ ImageViewApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image img;
+
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            img = Image.FromFile(files[0]);
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (img != null)
+            {
+                //e.Graphics.DrawImage(img, 0, 0, img.Width, img.Height);
+                RectangleF winRect = new RectangleF(0, 0, e.Graphics.VisibleClipBounds.Width, 
+                    e.Graphics.VisibleClipBounds.Height);
+                SizeF size = new SizeF(img.Width / img.HorizontalResolution, 
+                    img.Height / img.VerticalResolution);
+
+                float scale = Math.Min(winRect.Width / size.Width, winRect.Height / size.Height);
+
+                size.Width *= scale;
+                size.Height *= scale;
+                e.Graphics.DrawImage(img, winRect.X +
+                    (winRect.Width - size.Width) / 2, winRect.Y +
+                    (winRect.Height - size.Height) / 2, size.Width, size.Height);
+
+            }
+        }
+    }
+}
---
+++ Lab 29-30/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Lab_29_30
+{
+    public partial class Form1 : Form
+    {
+
+        ArrayList x, y;
+        bool doDraw = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            x = new ArrayList();
+            y = new ArrayList();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            doDraw = true;
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            doDraw = false;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (doDraw)
+            {
+                Graphics g = this.CreateGraphics();
+                SolidBrush redBrush = new SolidBrush(Color.Red);
+                g.FillRectangle(redBrush, e.X, e.Y, 2, 2);
+                x.Add(e.X);
+                y.Add(e.Y);
+                g.Dispose();
+                redBrush.Dispose();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush redBrush = new SolidBrush(Color.Red); //Цвет кисти
+            for (int i = 0; i < x.Count; i++)
+            {
+                g.FillRectangle(redBrush, (int)(x[i]), (int)(y[i]), 2, 2);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
